7b8747a9c19737782d521d583290ebeee2ceccb9
Bartosz Taczała <bartosz.taczala@siili.com>
2019-01-30 00:42:34 +0100

    Bump nettle to 3.4.1

--- scripts/nettle/compile_nettle.sh
+++ scripts/nettle/compile_nettle.sh
@@ -2,16 +2,18 @@
 #
 source ../common.sh
 
-if [ ! -d "nettle-3.4" ]; then 
-    wget https://ftp.gnu.org/gnu/nettle/nettle-3.4.tar.gz
-    tar xf nettle-3.4.tar.gz
+if [ ! -d "nettle-3.4.1" ]; then 
+    wget https://ftp.gnu.org/gnu/nettle/nettle-3.4.1.tar.gz
+    tar xf nettle-3.4.1.tar.gz
 fi
 
-cd nettle-3.4
+cd nettle-3.4.1
 GMP_LIBS="-L$tizen_root/platforms/tizen-4.0/wearable/rootstraps/wearable-4.0-device.core/usr/lib/engines/" \
 CC="$tizen_root/tools/arm-linux-gnueabi-gcc-6.2/bin/arm-linux-gnueabi-gcc --sysroot=$tizen_sysroot" \
 CXX="$tizen_root/tools/arm-linux-gnueabi-gcc-6.2/bin/arm-linux-gnueabi-g++ --sysroot=$tizen_sysroot" \
-./configure --host=arm-linux-gnueabi --prefix=$tizen_install_path
+./configure --host=arm-linux-gnueabi \
+    --prefix=$tizen_install_path \
+    --enable-mini-gmp
 make 
 make install
 cd -
