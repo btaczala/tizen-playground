a319f695afe39486dbf6b836e7b28f8d5f4b7fb6
Bartosz Taczała <bartosz.taczala@siili.com>
2019-01-30 00:43:19 +0100

    Override pkg-config env for cross compiling

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 tizen_root='/Users/bartosztaczala/tizen-studio'
-tizen_sysroot="$tizen_root/platforms/tizen-4.0/wearable/rootstraps/wearable-4.0-device.core/"
+tizen_sysroot="$tizen_root/platforms/tizen-4.0/wearable/rootstraps/wearable-4.0-device.core"
 tizen_install_path="$(git rev-parse --show-toplevel)/install"
+
+export PKG_CONFIG_DIR=""
+export PKG_CONFIG_LIBDIR="$tizen_install_path/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$tizen_sysroot"
